818f5fbfca5eb5dd160f33d3b99867b4e0a0422c
nikiiigoyal <nikiiigoyal@gmail.com>
2025-05-12 21:56:15 +0530

    Add new project for react -ts

---
+++ src/tutorial.ts
@@ -0,0 +1,11 @@
+let awesomeName: string = 'shakeAndBake';
+awesomeName = 'something';
+awesomeName = awesomeName.toUpperCase();
+
+console.log(awesomeName)
+
+let amount: number = 12;
+amount = 12 - 1;
+
+let isAwesome: boolean = true;
+isAwesome = false;
