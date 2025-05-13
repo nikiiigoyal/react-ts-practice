dcf8d7c93d8b36cf1ff2212d4d997c353a72fed3
nikiiigoyal <nikiiigoyal@gmail.com>
2025-05-13 15:20:09 +0530

    Add new file tutorial

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -9,3 +9,13 @@ amount = 12 - 1;
 
 let isAwesome: boolean = true;
 isAwesome = false;
+
+let greeting: string = 'Hello Typescript';
+greeting = greeting.toUpperCase();
+
+let age: number = 25;
+age = age + 5;
+
+let isAdult: boolean = age >= 18;
+isAdult = !isAdult;
+
